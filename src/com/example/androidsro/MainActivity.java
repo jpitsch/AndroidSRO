00e20d70b932a85817a614a55dd97581fc72ab68
JPitsch <jason_pitsch@spe.sony.com>
2015-02-04 09:45:43 -0800

    Added a test video playback

--- src/com/example/androidsro/MainActivity.java
+++ src/com/example/androidsro/MainActivity.java
@@ -1,16 +1,47 @@
 package com.example.androidsro;
 
 import android.app.Activity;
+import android.media.AudioManager;
+import android.media.MediaCodec;
+import android.media.MediaDrm;
+import android.media.MediaExtractor;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnPreparedListener;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
+//public class MainActivity extends Activity implements SurfaceHolder.Callback, OnPreparedListener{
 public class MainActivity extends Activity {
 
+	private MediaPlayer mediaPlayer;
+	private SurfaceHolder vidHolder;
+	private SurfaceView vidSurface;
+	String vidAddress = "https://s3-us-west-1.amazonaws.com/jwplayeroverlay/jw-text-overlay/test.mp4";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//vidSurface = (SurfaceView) findViewById(R.id.surfView);
+		//vidHolder = vidSurface.getHolder();
+		//vidHolder.addCallback(this);
+		
+		VideoView vidView = (VideoView)findViewById(R.id.myVideo);
+		MediaController vidControl = new MediaController(this);
+		vidControl.setAnchorView(vidView);
+		vidView.setMediaController(vidControl);
+		
+		Uri vidUri = Uri.parse(vidAddress);
+		
+		vidView.setVideoURI(vidUri);
+		vidView.start();
 	}
 
 	@Override
@@ -31,4 +62,42 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+
+//	@Override
+//	public void onPrepared(MediaPlayer mp) {
+//		
+//		mediaPlayer.start();
+//		
+//	}
+//
+//	@Override
+//	public void surfaceCreated(SurfaceHolder holder) {
+//		
+//		try {
+//		    mediaPlayer = new MediaPlayer();
+//		    mediaPlayer.setDisplay(vidHolder);
+//		    mediaPlayer.setDataSource(vidAddress);
+//		    mediaPlayer.prepare();
+//		    mediaPlayer.setOnPreparedListener(this);
+//		    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+//		} 
+//		catch(Exception e){
+//		    e.printStackTrace();
+//		}
+//		
+//	}
+//
+//	@Override
+//	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+//			int height) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void surfaceDestroyed(SurfaceHolder holder) {
+//		// TODO Auto-generated method stub
+//		
+//	}
 }
