6107a4b798cba9ded7b320a9360b84d4865c2f8f
JPitsch <jason_pitsch@spe.sony.com>
2015-02-06 10:05:13 -0800

    Committing changes I have to transfer to desktop.

--- gen/com/example/androidsro/R.java
+++ gen/com/example/androidsro/R.java
@@ -31,17 +31,24 @@ public final class R {
         public static final int myVideo=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_carousel_=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_video_playback=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int carousel_=0x7f070000;
+        public static final int main=0x7f070001;
+        public static final int video_playback=0x7f070002;
     }
     public static final class string {
+        public static final int action_settings=0x7f050006;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
         public static final int menu_carousel=0x7f050003;
         public static final int menu_settings=0x7f050002;
         public static final int menu_video=0x7f050004;
+        public static final int title_activity_carousel_=0x7f050005;
+        public static final int title_activity_video_playback=0x7f050007;
     }
     public static final class style {
         /** 
--- src/com/example/androidsro/MainActivity.java
+++ src/com/example/androidsro/MainActivity.java
@@ -1,47 +1,19 @@
 package com.example.androidsro;
 
 import android.app.Activity;
-import android.media.AudioManager;
-import android.media.MediaCodec;
-import android.media.MediaDrm;
-import android.media.MediaExtractor;
-import android.media.MediaPlayer;
-import android.media.MediaPlayer.OnPreparedListener;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.widget.MediaController;
-import android.widget.VideoView;
 
 //public class MainActivity extends Activity implements SurfaceHolder.Callback, OnPreparedListener{
 public class MainActivity extends Activity {
 
-	private MediaPlayer mediaPlayer;
-	private SurfaceHolder vidHolder;
-	private SurfaceView vidSurface;
-	String vidAddress = "https://s3-us-west-1.amazonaws.com/jwplayeroverlay/jw-text-overlay/test.mp4";
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		//vidSurface = (SurfaceView) findViewById(R.id.surfView);
-		//vidHolder = vidSurface.getHolder();
-		//vidHolder.addCallback(this);
-		
-		VideoView vidView = (VideoView)findViewById(R.id.myVideo);
-		MediaController vidControl = new MediaController(this);
-		vidControl.setAnchorView(vidView);
-		vidView.setMediaController(vidControl);
-		
-		Uri vidUri = Uri.parse(vidAddress);
-		
-		vidView.setVideoURI(vidUri);
-		vidView.start();
 	}
 
 	@Override
@@ -56,48 +28,22 @@ public class MainActivity extends Activity {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		
+		switch (item.getItemId()) {
+	        case R.id.action_settings:
+	            //showSettings();
+	            return true;
+	        case R.id.menu_carousel:
+	        	Intent carouselIntent = new Intent(this, CarouselActivity.class);
+	        	startActivity(carouselIntent);
+	            //showCarousel();
+	            return true;
+	        case R.id.menu_video:
+	        	Intent videoIntent = new Intent(this, VideoPlaybackActivity.class);
+	        	startActivity(videoIntent);
+	        	return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
 		}
-		return super.onOptionsItemSelected(item);
 	}
-	
-
-//	@Override
-//	public void onPrepared(MediaPlayer mp) {
-//		
-//		mediaPlayer.start();
-//		
-//	}
-//
-//	@Override
-//	public void surfaceCreated(SurfaceHolder holder) {
-//		
-//		try {
-//		    mediaPlayer = new MediaPlayer();
-//		    mediaPlayer.setDisplay(vidHolder);
-//		    mediaPlayer.setDataSource(vidAddress);
-//		    mediaPlayer.prepare();
-//		    mediaPlayer.setOnPreparedListener(this);
-//		    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-//		} 
-//		catch(Exception e){
-//		    e.printStackTrace();
-//		}
-//		
-//	}
-//
-//	@Override
-//	public void surfaceChanged(SurfaceHolder holder, int format, int width,
-//			int height) {
-//		// TODO Auto-generated method stub
-//		
-//	}
-//
-//	@Override
-//	public void surfaceDestroyed(SurfaceHolder holder) {
-//		// TODO Auto-generated method stub
-//		
-//	}
 }
